ee86d3907c00a8af0b7586383bda3d3259a31e75
Emiliano Righi <emiliano.righi@inmarsat.com>
2019-01-13 09:49:33 +0000

    parsing CSV

--- csvtojson.py
+++ csvtojson.py
@@ -2,15 +2,30 @@
 import csv
 import json
 
-targets = dict()
+targets = []
+
 
 with open("node_exporter.csv", "rb") as nodeexfile:
     reader = csv.reader(nodeexfile)
     next(reader, None)
-    new_env = 1
     for row in reader:
+        new_env = 0
+        idx = 0
+        element = dict()
         for idx, element in enumerate(targets):
-            if element["env"] == row[1]:
-                print(idx, element)
-                new_env = 0
+            if element["labels"]["env"] == row[1]:
+                new_env = 1
+                new_service = 0
+#                if element["labels"]["service"] == row[3]:
+#                    print(row[3])
+#                else:
+#                    element["labels"]["service"] = row[3]
+#                    print(element["service"])
 
+        if new_env == 0:
+            element["labels"]["env"] = row[1]
+            targets.append(element["labels"]["env"])
+            print(element["labels"]["env"])
+            targets[idx] = element
+
+        print(targets)
