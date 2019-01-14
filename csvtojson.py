ad68a4fed08b308a49b1feb8050f395dbb6c1242
Emiliano Righi <emiliano.righi@inmarsat.com>
2019-01-14 15:20:02 +0000

    csvtojson working

--- csvtojson.py
+++ csvtojson.py
@@ -2,30 +2,41 @@
 import csv
 import json
 
-targets = []
+targetarray = []
 
 
 with open("node_exporter.csv", "rb") as nodeexfile:
     reader = csv.reader(nodeexfile)
     next(reader, None)
     for row in reader:
-        new_env = 0
+        new_target = 0
         idx = 0
+        new_element = dict()
         element = dict()
-        for idx, element in enumerate(targets):
-            if element["labels"]["env"] == row[1]:
-                new_env = 1
-                new_service = 0
-#                if element["labels"]["service"] == row[3]:
-#                    print(row[3])
-#                else:
-#                    element["labels"]["service"] = row[3]
-#                    print(element["service"])
+        for idx, element in enumerate(targetarray):
+            if element['labels']['env'] == row[1]:
+                new_target = 1
+                if element['labels']['job'] == row[2]:
+                    if element['labels']['service'] == row[3]:
+                        new_target = 1
+                        if row[0] not in element['targets']:
+                            print(row[0])
+                            element["labels"] = {'env': row[1], 'job': row[2], 'service': row[3]}
+                            element['targets'].append(row[0])
 
-        if new_env == 0:
-            element["labels"]["env"] = row[1]
-            targets.append(element["labels"]["env"])
-            print(element["labels"]["env"])
-            targets[idx] = element
 
-        print(targets)
+                    else:
+                        new_target = 0
+                else:
+                    new_target = 0
+
+        if new_target == 0:
+            new_element["labels"] = {'env': row[1], 'job': row[2], 'service': row[3]}
+            if 'targets' not in new_element['labels']:
+                new_element['targets'] = []
+            new_element['targets'].append(row[0])
+
+            targetarray.append(new_element)
+
+for dictrow in targetarray:
+    print(json.dumps(dictrow))
